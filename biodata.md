916772273397b7bd96ce87b5dbcd81eaa928dbc6
maghfiraathiyahadhavi <maghfira.athiyah6717@student.unri.ac.id>
2026-01-29 10:36:06 +0700

    menambahkan biodata.py

---
+++ biodata.md
@@ -0,0 +1,3 @@
+Nama    : Maghfira Athiyah Ad-Havi 
+Nim     : 25071206717
+Prodi   : Teknik Informatika`
---
+++ biodata.py
@@ -0,0 +1,4 @@
+print("Maghfira Athiyah Ad-Havi")
+print("2507126717")
+print("Teknik Informatika")
+print("Hello World")
