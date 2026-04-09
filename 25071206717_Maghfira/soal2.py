49a26d1b549bdec214c99a5b8944d578185d6d77
maghfiraathiyahadhavi <maghfira.athiyah6717@student.unri.ac.id>
2026-04-09 09:34:34 +0700

    update

--- 25071206717_Maghfira/soal2.py
+++ 25071206717_Maghfira/soal2.py
@@ -41,3 +41,43 @@ def filter_belum_bayar(): #list pasien yg belum bayar
 
 tampilkan_pasien() #memanggil fungsi untuk output
 filter_belum_bayar()
+
+
+def info_klinik(): #tuple
+    klinik = (
+        "Klinik Sehat Bersama",
+        "Jl. Merdeka No. 10, Pekanbaru",
+        "0761-12345"
+    )
+    
+    print("Info Klinik:")
+    print("Nama   :", klinik[0])
+    print("Alamat :", klinik[1])
+    print("Telp   :", klinik[2])
+    
+def rekap_penyakit(): #rekap jenis penyakit dengan set
+    penyakit_unik = {p["penyakit"] for p in pasien_hari_ini} # set
+    
+    print("\nJenis Penyakit Unik:", penyakit_unik)
+    print("Jumlah jenis penyakit:", len(penyakit_unik))
+    
+    rekap = {} # menghitung jumlah pasien per penyakit dengan dictionary
+    for p in pasien_hari_ini: #loop
+        penyakit = p["penyakit"]
+        if penyakit in rekap:
+            rekap[penyakit] += 1
+        else:
+            rekap[penyakit] = 1
+    
+    print("\nRekap per penyakit:") #menampilkan hasil rekap
+    for penyakit in rekap:
+        print(f"{penyakit} : {rekap[penyakit]} pasien")
+    
+    maks = max(rekap.values()) #pasien jumlah terbanyak
+    
+    terbanyak = [p for p in rekap if rekap[p] == maks] # penyakit dengan jumlah terbanyak
+    
+    print("\nPenyakit terbanyak:", ", ".join(terbanyak), f"({maks} pasien)")
+    
+info_klinik() #memanggil fungsi untuk  menampilkan output
+rekap_penyakit()
